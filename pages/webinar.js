99df6041e76e18a282fa6516707d861db8fe843d
Samet Gölgeci <samet.golgeci@gmail.com>
2021-01-25 17:47:57 +0300

    Webinar.js Update
    - Webinar sayfa tasarımı değiştirildi

--- pages/webinar.js
+++ pages/webinar.js
@@ -13,22 +13,30 @@ const Webinar = () => (
 	    		{/*<p><a href="https://us02web.zoom.us/meeting/register/tZMvcOispjkuGt3Xv2sm9Pmj2WdlLTikMRwH"><img className="webinar-afis" src="/img/webinar/webinar_afis_4.jpg"></img></a></p>*/}
 	    	</div>
 	    </div>
-	    <div className="webinar-bottom-up">
-	    	<div className="webinar-bottom-up-title">- Uzaktan Eğitimde Öğrenci Merkezli Eğitimi Başarabilir Miyiz (22 Temmuz 2020) -</div>
+	    <div className="webinar-bottom">
 	    	<iframe width="560" height="315" src="https://www.youtube.com/embed/VD9C3UzderE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	    	<div className="webinar-bottom-title">- Uzaktan Eğitimde Öğrenci Merkezli Eğitimi Başarabilir Miyiz (22 Temmuz 2020) -</div>
 	    </div>
-	    <div className="webinar-bottom-up">
-	    	<div className="webinar-bottom-up-title">- Pandemi Sürecinde Hemşirelik Eğitimi (Teorik ve Uygulama): ABD Örnekleri (13 Temmuz 2020) -</div>
-	    	<iframe width="560" height="315" src="https://www.youtube.com/embed/eh-QFSJ-WuE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	    </div>
-	    <div className="webinar-bottom-up">
-	    	<div className="webinar-bottom-up-title">- Pandemi Sürecinde Üniversitelerde Dijital Dönüşüm (30 Haziran 2020) -</div>
-	    	<iframe width="560" height="315" src="https://www.youtube.com/embed/2t3_5Sbsgi8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	    </div>
-	    <div className="webinar-bottom-up">
-	    	<div className="webinar-bottom-up-title">- Covid-19 Sürecinde Hemşirelik Eğitimi (17 Haziran 2020) -</div>
-	    	<iframe width="560" height="315" src="https://www.youtube.com/embed/BGs8vR3mzbE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	    </div>
+	    <div className="row">
+	    	<div className="col-md-4">
+		   		<div className="embed-responsive embed-responsive-16by9">
+					  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/eh-QFSJ-WuE" allowFullScreen></iframe>
+					</div>
+					<div className="webinar-bottom-title">Pandemi Sürecinde Hemşirelik Eğitimi (Teorik ve Uygulama): ABD Örnekleri (13 Temmuz 2020)</div>
+	     	</div>
+      	<div className="col-md-4">
+		     	<div className="embed-responsive embed-responsive-16by9">
+					  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/2t3_5Sbsgi8" allowFullScreen></iframe>
+					</div>
+					<div className="webinar-bottom-title">Pandemi Sürecinde Üniversitelerde Dijital Dönüşüm (30 Haziran 2020)</div>
+	     	</div>
+	     	<div className="col-md-4">
+		   		<div className="embed-responsive embed-responsive-16by9">
+					  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/BGs8vR3mzbE" allowFullScreen></iframe>
+					</div>
+					<div className="webinar-bottom-title">Covid-19 Sürecinde Hemşirelik Eğitimi (17 Haziran 2020)</div>
+	     	</div>
+			</div>
     </MasterPage>
 )
 export default Webinar
