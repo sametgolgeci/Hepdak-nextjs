50c08f58e74a505a98fca279847cebe16703c3d9
Samet Gölgeci <samet.golgeci@gmail.com>
2021-03-24 12:25:48 +0300

    v.2.21
    - Yeni webinar dosyası siteye eklendi

--- pages/webinar.js
+++ pages/webinar.js
@@ -14,10 +14,22 @@ const Webinar = () => (
 	    	</div>
 	    </div>
 	    <div className="webinar-bottom">
+	  {/*
 	    	<iframe width="560" height="315" src="https://www.youtube.com/embed/VD9C3UzderE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	    	<div className="webinar-bottom-title">- Uzaktan Eğitimde Öğrenci Merkezli Eğitimi Başarabilir Miyiz (22 Temmuz 2020) -</div>
+	  */}
+		  	<Link href="/doc/webinar/webinar-1.pptx"><a target="_blank">
+		  		<img src="/doc/webinar/webinar-1.png" alt=""/>
+		  		<div className="webinar-bottom-title">- Avustralya Hemşirelik ve Ebelik Akreditasyon Derneği ve Hemşirelik Okullarının Yükümlülükleri -</div>
+		  	</a></Link>
 	    </div>
 	    <div className="row">
+	    	<div className="col-md-4">
+		   		<div className="embed-responsive embed-responsive-16by9">
+					  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/VD9C3UzderE" allowFullScreen></iframe>
+					</div>
+					<div className="webinar-bottom-title">Uzaktan Eğitimde Öğrenci Merkezli Eğitimi Başarabilir Miyiz (22 Temmuz 2020)</div>
+	     	</div>
 	    	<div className="col-md-4">
 		   		<div className="embed-responsive embed-responsive-16by9">
 					  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/eh-QFSJ-WuE" allowFullScreen></iframe>
