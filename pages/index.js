466eef5c8fc7f27b4b937e19db31a89af295f87c
Samet Gölgeci <samet.golgeci@gmail.com>
2021-05-22 13:56:15 +0300

    v.2.35
    - Yeni duyuru eklendi
    - Uluslararası Toplantılar 1 fotoları eklendi
    - Uluslararası Toplantılar 2 eklendi
    - b7_v5_1 dosyası değiştirildi

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,9 @@ const Anasayfa = () => (
             <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
               <div className="carousel-inner">
                 <div className="carousel-item active">
+                    <div className="main-top-one-content">HEPDAK Değerlendiricilerimizden ve aynı zamanda HEPDAK Uluslararası Dış Değerlendirme Çalışma Grubu üyelerimizden Doç.Dr.Sergül Duygulu ve Doç.Dr.Fatoş Korkmaz HEPDAK’ın üyesi olduğu CEENQA’nın (Orta ve Doğu Avrupa Yükseköğretim Kalite Güvence Ajansları Ağı) bir diğer üyesi olan ve HEPDAK’ın Değerlendirici Değişim Programı (CEENQA Staff Exchange Program) gerçekleştirdiği Letonya Akreditasyon Ajansı AIC (Academic Information Center)-/AIKA (Quality Agency for Higher Education ) yönetim kurulu tarafından 12.05.2021 tarihinde Dış Değerlendirici olarak seçilmiştir. Değerlendirici hocalarımızı tebrik eder başarılı çalışmalar yürütmelerini dileriz.</div>
+                </div>
+                <div className="carousel-item">
                     <div className="main-top-one-content-full"><img className="slider-full-img" src="img/duyurular/hemsire2021.png"/></div>
                 </div>
                 <div className="carousel-item">
@@ -184,6 +187,10 @@ const Anasayfa = () => (
           </ul>
           <div className="tab-content">
             <div id="yirmibir" className="container tab-pane active">
+              <div className="main-top-second">
+                <div className="main-top-second-title">DUYURU</div>
+                <div className="main-top-second-content">HEPDAK Değerlendiricilerimizden ve aynı zamanda HEPDAK Uluslararası Dış Değerlendirme Çalışma Grubu üyelerimizden Doç.Dr.Sergül Duygulu ve Doç.Dr.Fatoş Korkmaz HEPDAK’ın üyesi olduğu CEENQA’nın (Orta ve Doğu Avrupa Yükseköğretim Kalite Güvence Ajansları Ağı) bir diğer üyesi olan ve HEPDAK’ın Değerlendirici Değişim Programı (CEENQA Staff Exchange Program) gerçekleştirdiği Letonya Akreditasyon Ajansı AIC (Academic Information Center)-/AIKA (Quality Agency for Higher Education ) yönetim kurulu tarafından 12.05.2021 tarihinde Dış Değerlendirici olarak seçilmiştir. Değerlendirici hocalarımızı tebrik eder başarılı çalışmalar yürütmelerini dileriz.<p><h6>HEPDAK Yönetim Kurulu</h6></p></div>
+              </div>
               <div className="main-top-second">
                 <div className="main-top-second-title">12-18 Mayıs 2021 Hemşirelik Haftamız Kutlu Olsun.</div>
                 <div className="main-top-second-content"><img src="img/duyurular/hemsire2021.png"/></div>
--- pages/uluslararasi-toplantilar.js
+++ pages/uluslararasi-toplantilar.js
@@ -10,6 +10,11 @@ const Calistay = () => (
 	<div className="row">
 		<div className="genel col-md-12 col-sm-12">
 		<h1>Uluslararası Toplantılar</h1>
+		<h6><span className="badge badge-custom">2</span> 19 Mayıs 2021 - CEENQA Toplantısı </h6>
+			<p className="madde-p">
+				<a href="/doc/uluslararasi-2/belge_1.pdf" target="_blank">1. 19 May 2021: Fifth bi-monthly CEENQA meeting</a><br/>
+				<a href="/doc/uluslararasi-2/belge_2.pdf" target="_blank">2. GESIQA</a><br/>
+			</p>
 		<h6><span className="badge badge-custom">1</span> CEENQA Online Staff Exchange Program</h6>
 			<p className="madde-p">
 				<a href="/doc/uluslararasi-1/belge_1.pdf" target="_blank">1. CEENQA Online Staff Exchange Program</a><br/>
@@ -17,14 +22,31 @@ const Calistay = () => (
 				<a href="/doc/uluslararasi-1/belge_3.pdf" target="_blank">3. HEPDAK : Response to COVID-19 Pandemic QUALITY ASSURANCE ACTIVITIES OF HEPDAK FOR DISTANCE EVALUATION</a><br/>
 				<a href="/doc/uluslararasi-1/belge_4.pdf" target="_blank">4. Council of Higher Education - Turkey</a><br/>
 			</p>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-1.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-1.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-2.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-2.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-3.png" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-3.png"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-4.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-4.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-5.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-5.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-6.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-6.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-7.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-7.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-8.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-8.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-9.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-9.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-10.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-10.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-11.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-11.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-12.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-12.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-13.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-13.jpg"/></a></div>
+				</div>
+			</div>
 		{/*
 		<p>
 			<ul className="nav calistay-nav">
 			  <li className="nav-item">
-			    <a className="nav-link calistay-button" href="doc/ogrenci_egitim_4/program.pdf" target="_blank">Program</a>
+			    <a className="nav-link calistay-button" href="doc/uluslararasi-1/foto-program.pdf" target="_blank">Program</a>
 			  </li>
 			  <li className="nav-item">
-			    <a className="nav-link calistay-button" href="doc/ogrenci_egitim_4/katilimcilar.pdf" target="_blank">Katılımcı Listesi</a>
+			    <a className="nav-link calistay-button" href="doc/uluslararasi-1/foto-katilimcilar.pdf" target="_blank">Katılımcı Listesi</a>
 			  </li>
 			  <li className="nav-item">
 			    <a className="nav-link calistay-button" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Sunumlar</a>
@@ -44,43 +66,42 @@ const Calistay = () => (
 		<div className="collapse" id="collapseExample">
 			<p><strong>-> Sunumlar</strong></p>
 			<p className="madde-p">
-				<a href="/doc/ogrenci_egitim_4/belge_1.ppt">1. Çalıştayın Amaçları</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_2.ppt">2. Akreditasyon Süreci</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_3.ppt">3. HEPDAK  Hemşirelik Lisans Eğitim Programı Standartları (Sürüm 4.0)</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_4.ppt">4. Öğrenci Değerlendirici Standartları</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_5.ppt">5. Öğrenci Değerlendiricilerin Sürece Katılımı</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_6.ppt">6. Davranışsal Boyut</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_7.ppt">7. Tutarlılık</a><br/>
-				<a href="/doc/ogrenci_egitim_4/belge_8.ppt">8. Rapor Yazımı</a>
+				<a href="/doc/uluslararasi-1/foto-belge_1.ppt">1. Çalıştayın Amaçları</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_2.ppt">2. Akreditasyon Süreci</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_3.ppt">3. HEPDAK  Hemşirelik Lisans Eğitim Programı Standartları (Sürüm 4.0)</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_4.ppt">4. Öğrenci Değerlendirici Standartları</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_5.ppt">5. Öğrenci Değerlendiricilerin Sürece Katılımı</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_6.ppt">6. Davranışsal Boyut</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_7.ppt">7. Tutarlılık</a><br/>
+				<a href="/doc/uluslararasi-1/foto-belge_8.ppt">8. Rapor Yazımı</a>
 			</p>
 		</div>
 		<div className="collapse" id="collapseExampleFoto">
 			<p><strong>-> Fotoğraflar</strong></p>
 			<div className="container">
 				<div className="row">
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/1.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/1.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/2.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/2.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/3.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/3.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/4.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/4.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/5.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/5.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/6.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/6.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/7.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/7.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/8.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/8.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/9.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/9.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/10.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/10.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/11.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/11.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/12.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/12.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/13.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/13.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/14.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/14.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/15.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/15.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/ogrenci_egitim_4/16.jpg" target="_blank"><img className="calistay-img" src="/doc/ogrenci_egitim_4/16.jpg" alt="Öğrenci Eğitimi 28 Mart 2021"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-1.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-1.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-2.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-2.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-3.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-3.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-4.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-4.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-5.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-5.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-6.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-6.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-7.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-7.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-8.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-8.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-9.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-9.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-10.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-10.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-11.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-11.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-12.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-12.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-13.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-13.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-14.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-14.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-15.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-15.jpg"/></a></div>
+					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-16.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-16.jpg"/></a></div>
 				</div>
 			</div>
 		</div>
 		*/}
 		</div>
 	</div>
-	<div className="safe"></div>
 </MasterPage>
 )
 export default Calistay
