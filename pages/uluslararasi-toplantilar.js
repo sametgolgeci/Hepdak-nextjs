711c65da089ac1209689532d2d4d6cb567316705
Samet Gölgeci <samet.golgeci@gmail.com>
2022-02-04 17:08:52 +0300

    v.4.13
    - Uluslararası Toplantılar sayfası tasarımı değiştirildi

--- pages/uluslararasi-toplantilar.js
+++ pages/uluslararasi-toplantilar.js
@@ -11,46 +11,112 @@ const Calistay = () => (
 		<div className="genel col-md-12 col-sm-12">
 		<h1>Uluslararası Toplantılar ve Faaliyetler</h1>
 		<h4>- CEENQA Faaliyetleri</h4>
+		<h6><span className="badge badge-custom">-</span> CEENQA Haberler </h6>
+			<div className="madde-p">
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA13ekim2021" role="button" aria-expanded="false" aria-controls="collapseCEENQA13ekim2021">- CEENQA'nın Yıllık Genel Kurulu ve Çalıştayı (13 Ekim 2021)</a>
+				<div className="collapse" id="collapseCEENQA13ekim2021">
+					<p>CEENQA'nın yıllık Genel Kurulu ve beraberindeki çalıştayı 13 Ekim 2021'de online gerçekleşmiştir.Toplantıya HEPDAK’ı temsilen Prof. Dr. Ayla BAYIK TEMEL, Doç. Dr. Sergül DUYGULU katılmıştır. CEENQA yeni dönem yönetim kurulu üye seçimi için tam üye olarak oylamaya katılım sağlanmıştır. Genel Kurulda Olgun Çiçek (YÖDAK, Kuzey Kıbrıs) CEENQA'nın yeni Başkan Yardımcısı, Simona Lache (ARACIS, Romanya) ve Benjamin Muhamedbegoviç (HEA, Bosna Hersek) yeni yönetim kurulu üyeleri olarak seçilmişlerdir.</p>
+					<p>“Çevrimiçi Değerlendirmeler ve Dış Kalite Güvencesi” konulu çalıştayda, pandeminin getirdiği değerlendirme prosedürlerinde yapılan değişiklikler ele alınmıştır. Çeşitli Avrupa ülkelerindeki ajansların deneyimlerine dayanarak, hangi deneyimlerin kazanıldığı, değişen koşullara uyum sağlamak için nelerin geliştirilebileceği ve gelecekte kalite güvence prosedürlerinin nasıl yürütüleceği tartışılmıştır. Çalıştayda bu kapsamda Anca Prisacariu “Dış Değerlendirme Güvenliği Süreçlerinin Online Formatta Uyarlanması” konulu sunumunda online değerlendirmede sınırlılıklar, etik durumlar konusunda görüşlerini paylaşmıştır. Nataliia Stukalo, NAQA’nin Ukrayna’da pandemi döneminde kalite güvenliği ile ilgili deneyimlerini açıklamış, bu kapsamda olumlu ve olumsuz denyimler, kalite güvenliği süreçlerinde dijitalizasyon konularında bilgi vermiştir. Klemen Šubic CEENQA adına Olgun Çiçek ile birlikte hazırladıkları HEPDAK’ında öneriler verdiği “Uzaktan Değerlendirmeler İçin Rehber”in (Guidelines For Distance Evaluation) hazırlanma amacı, online değerlendirmede ziyaret öncesi ve ziyaret sürecinde dikkat edilecek hususlar hakkında bilgi vermiştir. Jolanta Silka (AIKA) sunumunda online değerlendirmelerde iletişimin ve geri bildirimin önemi konusunda bilgiler vermiştir. Ayrıca genel kurulda Türkiye’den DEPAD (Diş Hekimliği Eğitimi Programları Akreditasyon Derneği) gözlemci üye ve Azerbeycan’dan TKTA (Azerbaycan Kalite Ajansı) – tam üyelik başvurusu kabul edilmiştir.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA6" role="button" aria-expanded="false" aria-controls="collapseCEENQA6">- CEENQA Paylaşım Toplantısı - 6 (15 Eylül 2021)</a>
+				<div className="collapse" id="collapseCEENQA6">
+					<p>Kalite güvencesi alanında CEENQA’ya üye ajansların kurumlarını ve faaliyetlerini tanıttığı, yenilikçi çalışmaların paylaşıldığı periyodik toplantıların yedincisi 15 Eylül 2021 gerçekleştirilmiştir. Çevrimiçi gerçekleştirilen toplantıya HEPDAK’ı temsilen Prof. Dr. Ayla BAYIK TEMEL, Doç. Dr. Sergül DUYGULU, Doç.Dr. Merlinda ALUŞ TOKAT ve Doç. Dr Fatoş KORKMAZ katılmıştır.</p>
+					<p>Toplantıda Stela Guvir,Moldova Cumhuriyeti eğitim ve Araştırma Kalite Güvenliği Ajansı (Ulusal National Agency for Quality Assurance in Education and Research, Republic of Moldova) (ANACIP-ANACEC) ile ilgili organizasyon yapısı, görevler, dış değerlendirme süreci ve standartları, uluslararası işbirliği ve tanınırlık, projeleri konusunda bilgiler vermiştir. Emita Blackdan, Hırvatistan Yüksek Öğrenim ve Bilim için Kalite Ajansı ve Kalite Güvenliği Sistemi (Croatian Agency for Science and Higher Education and QA System in Croatia) AZVO -ASHE ile ilgili olarak Hırvatistan’da yüksek öğretim sistemi, kalite güvenlik sistemi,faaliyetleri, misyon, vizyon ve stratejileri, pandemi sürecinde iş organizasyonu ve geleceğe yönelik planlamalar, uluslararası ilişkileri konusunda açıklamalar yapmıştır.</p>
+					<p>Colin Tück, Avrupa Yüksek Öğrenim İçin Kalite Güvenliği Ağı (European Quality Assurance Register for Higher Education) (EQAR DEQAR) temsilen Avrupa Yüksek Öğrenim Alanı ve Bologna Süreci (The European Higher Education Area -EHEA) kapsamında uluslararası şeffaflık ve otomatik tanınmanın desteklenmesi konusunda bilgiler vermiştir. Sunumlar ile ilgili ayrıntılı bilgilere <a href="https://www.ceenqa.org/ceenqa-bi-monthly-meetings/">https://www.ceenqa.org/ceenqa-bi-monthly-meetings/</a> linkinden ulaşabilir.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA5" role="button" aria-expanded="false" aria-controls="collapseCEENQA5">- CEENQA Paylaşım Toplantısı - 5 (21 Temmuz 2021)</a>
+				<div className="collapse" id="collapseCEENQA5">
+					<p>Kalite güvencesi alanında CEENQA’ya üye ajansların kurumlarını, faaliyetlerini tanıttığı ve
+yenilikçi çalışmaların periodik olarak paylaşıldığı altıncı topplantı 21 Temmuz 2021 tarihinde
+çevrimiçi olarak gerçekleştirildi.HEPDAK’ı temsilen bu online toplantıya Prof. Dr. Ayla Bayık
+Temel, Doç. Dr. Sergül Duygulu, Doç. Dr Fatoş Korkmaz ve Doç. Dr. Merlinda Aluş Tokat
+katılmışlardır. Bu toplantıda Doç. Dr. Sergül Duygulu HEPDAK’IN misyonu, amaçları, değerleri,
+yapısı program değerlendirme süreçleri, standartları ve geleceğe yönelik planlarını
+açıklamıştır. Nataliia Stukalo Ukrayna ‘daYüksek Öğrenimde Kalite Güvenliği konulu
+sunumunda; NAQA ajansının misyonu, amaçları, Ukrayna’da yüksek öğrenim, ajansın
+üyelikleri, Covid 19 pandemi sürecinde faaliyetler ve ülkeler arası faaliyetleri hakkından
+açıklamalar yapmıştır. Prof. Dr. Ayla Bayık Temel uzaktan değerlendirme ve uzaktan ziyaretler
+açısından iyi uygulama örneği olarak HEPDAK’ın değerlendirmeciler için düzenlediği uzaktan
+eğitim çalıştayı hakkında bilgiler vermiştir. İlgili sunumlara HEPDAK web sayfası yayınlar başlığı
+altından ulaşabilirsiniz.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA4" role="button" aria-expanded="false" aria-controls="collapseCEENQA4">- CEENQA Paylaşım Toplantısı - 4 (19 Mayıs 2021)</a>
+				<div className="collapse" id="collapseCEENQA4">
+					<p>CEENQA tarafından 2021 yılı etkinlikleri kapsamında iki ayda bir yürütülen 5. bilimsel toplantı
+19 Mayıs 2021 tarihinde gerçekleştirilmiş olup, toplantıya HEPDAK’ı temsilen Prof.Dr. Ayla
+Bayık Temel, Doç. Dr. Sergül Duygulu ve Doç. Dr. Fatoş Korkmaz katılmışlardır.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA3" role="button" aria-expanded="false" aria-controls="collapseCEENQA3">- CEENQA Paylaşım Toplantısı - 3 (19 Ocak 2021)</a>
+				<div className="collapse" id="collapseCEENQA3">
+					<p>19 Ocak 2021 tarihinde düzenlenen CEENQA’nın iki aylık toplantılarının üçüncüsüne
+HEPDAK ‘ı temsilen Prof. Dr. Ayla Bayık Temel ve Prof. Dr. Medine Yılmaz katılmıştır. ASIIN-
+Almanya temsilcisi Dr. Iring Wasser ve AIKA- Letonya temsilcisi Jolanta Silka faaliyetlerine
+ilişkin sunum yapmışlardır. ASIIN Ajansından Arne Thielenhaus veri ve kayıt yönetim sistemi
+ile ilgili kullandıkları kapsamlı çok dilli bilgisayar programı hakkında bilgi vermiştir.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA2" role="button" aria-expanded="false" aria-controls="collapseCEENQA2">- CEENQA Paylaşım Toplantısı - 2 (17 Kasım 2020)</a>
+				<div className="collapse" id="collapseCEENQA2">
+					<p>İki ayda bir düzenlenen CEENQA toplantılarının ikincisi 17 Kasım 2020 tarihinde çevrimiçi
+olarka gerçekleştirilmiştir. HEPDAK’ı temsilen toplantıya Prof. Dr. Ayla Bayık Temel, Prof.
+Dr. Hülya Okumuş ve Prof. Dr. Medine Yılmaz katılmışlardır. Toplantıda Çek Cumhuriyetinden
+NAB adına Martina Vidlakova ve Sırbistan’dan AHERS ajansı adına Duska Radmonavic sunum
+yapmışlardır.</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseCEENQA1" role="button" aria-expanded="false" aria-controls="collapseCEENQA1">- CEENQA Paylaşım Toplantısı - 1 (15 Ekim 2020)</a>
+				<div className="collapse" id="collapseCEENQA1">
+					<p>CEENQA genel kurulunda alınan karar doğrultusunda iki ayda bir düzenlenecek
+olan paylaşım toplantılarının ilki 15 Ekim 2020 tarihinde düzenlenmiş olup
+HEPDAK’ı temsilen Prof. Dr. Ayla Bayık Temel ve Prof. Dr. Hülya Okumuş
+katılmışlardır. Toplantıda QAA (NAKVIS), Slovenya’dan başkan Franci Demšar ve
+Kuzey Kıbrıs YÖDAK’ temsilen Akile Gürsoy sunumlar yapmışlardır.</p>
+				</div>
+			</div>
 		<h6><span className="badge badge-custom">-</span> 19 Mayıs 2021 - CEENQA Toplantısı </h6>
 			<p className="madde-p">
-				<a href="/doc/uluslararasi-2/belge_1.pdf" target="_blank">1. 19 May 2021: Fifth bi-monthly CEENQA meeting</a><br/>
+				<a href="/doc/uluslararasi-2/belge_1.pdf" target="_blank">- 19 May 2021: Fifth bi-monthly CEENQA meeting</a><br/>
 			</p>
 		<h6><span className="badge badge-custom">-</span> CEENQA Çevrimiçi Değerlendirici Değişim Programı</h6>
-			<p><strong>- Sunumlar</strong></p>
-			<p className="madde-p">
-				<a href="/doc/uluslararasi-1/belge_1.pdf" target="_blank">1. CEENQA Online Staff Exchange Program</a><br/>
-				<a href="/doc/uluslararasi-1/belge_2.pdf" target="_blank">2. HEPDAK Association for Evaluation and Accreditation of Nursing Education Programs</a><br/>
-				<a href="/doc/uluslararasi-1/belge_3.pdf" target="_blank">3. HEPDAK : Response to COVID-19 Pandemic QUALITY ASSURANCE ACTIVITIES OF HEPDAK FOR DISTANCE EVALUATION</a><br/>
-				<a href="/doc/uluslararasi-1/belge_4.pdf" target="_blank">4. Council of Higher Education - Turkey</a><br/>
-				<a href="/doc/uluslararasi-1/belge_5.pdf" target="_blank">5. Distance Evaluation AIC AIKA</a><br/>
-				<a href="/doc/uluslararasi-1/belge_6.pdf" target="_blank">6. Introduction AIC AIKA</a><br/>
-				<a href="/doc/uluslararasi-1/belge_7.pdf" target="_blank">7. Stakeholders AIC AIKA</a><br/>
-				<a href="/doc/uluslararasi-1/belge_8.pdf" target="_blank">8. Steps of assessment procedures AIC AIKA</a><br/>
-			</p>
-			<p><strong>- Fotoğraflar</strong></p>
-			<div className="container">
-				<div className="row">
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-1.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-1.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-2.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-2.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-3.png" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-3.png"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-4.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-4.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-5.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-5.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-6.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-6.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-7.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-7.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-8.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-8.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-9.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-9.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-10.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-10.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-11.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-11.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-12.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-12.jpg"/></a></div>
-					<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-13.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-13.jpg"/></a></div>
+			<div className="madde-p">
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">- Sunumlar</a>
+				<div className="collapse" id="collapseExample">
+					<p className="madde-p">
+						<a href="/doc/uluslararasi-1/belge_1.pdf" target="_blank">- CEENQA Online Staff Exchange Program</a><br/>
+						<a href="/doc/uluslararasi-1/belge_2.pdf" target="_blank">- HEPDAK Association for Evaluation and Accreditation of Nursing Education Programs</a><br/>
+						<a href="/doc/uluslararasi-1/belge_3.pdf" target="_blank">- HEPDAK : Response to COVID-19 Pandemic QUALITY ASSURANCE ACTIVITIES OF HEPDAK FOR DISTANCE EVALUATION</a><br/>
+						<a href="/doc/uluslararasi-1/belge_4.pdf" target="_blank">- Council of Higher Education - Turkey</a><br/>
+						<a href="/doc/uluslararasi-1/belge_5.pdf" target="_blank">- Distance Evaluation AIC AIKA</a><br/>
+						<a href="/doc/uluslararasi-1/belge_6.pdf" target="_blank">- Introduction AIC AIKA</a><br/>
+						<a href="/doc/uluslararasi-1/belge_7.pdf" target="_blank">- Stakeholders AIC AIKA</a><br/>
+						<a href="/doc/uluslararasi-1/belge_8.pdf" target="_blank">- Steps of assessment procedures AIC AIKA</a><br/>
+					</p>
+				</div>
+				<a className="uluslararasi-link" data-toggle="collapse" href="#collapseExampleFoto" role="button" aria-expanded="false" aria-controls="collapseExampleFoto">- Fotoğraflar</a>
+				<div className="collapse" id="collapseExampleFoto">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-1.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-1.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-2.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-2.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-3.png" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-3.png"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-4.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-4.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-5.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-5.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-6.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-6.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-7.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-7.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-8.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-8.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-9.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-9.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-10.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-10.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-11.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-11.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-12.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-12.jpg"/></a></div>
+							<div className="col-md-3 col-sm-12"><a href="/doc/uluslararasi-1/foto-13.jpg" target="_blank"><img className="calistay-img" src="/doc/uluslararasi-1/foto-13.jpg"/></a></div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<h6><span className="badge badge-custom">-</span> Uluslararası Çalışmalar </h6>
-			<p className="madde-p">
-				<a href="/doc/hepdak_sunumlari/belge_1.pdf" target="_blank">3. Prof.Dr. Ayla Bayık Temel - CEENQUA 2020 Aralık 2020 Bülten Yazısı</a><br/>
-				<a href="/doc/hepdak_sunumlari/belge_3.pdf" target="_blank">2. Prof.Dr. Ayla Bayık Temel - CEENQUA 2020 Temmuz Bülten Yazısı</a><br/>
-				<a href="/doc/Uluslararasi_Calismalar.pdf" target="_blank">1. HEPDAK’ın Üye Olduğu CEENQA İçindeki Faaliyetleri: 2020</a><br/>
-			</p>
+			<div className="madde-p">
+				<a href="/doc/hepdak_sunumlari/belge_1.pdf" className="uluslararasi-link" target="_blank">- Prof.Dr. Ayla Bayık Temel - CEENQUA 2020 Aralık 2020 Bülten Yazısı</a>
+				<a href="/doc/hepdak_sunumlari/belge_3.pdf" className="uluslararasi-link" target="_blank">- Prof.Dr. Ayla Bayık Temel - CEENQUA 2020 Temmuz Bülten Yazısı</a>
+				<a href="/doc/Uluslararasi_Calismalar.pdf" className="uluslararasi-link" target="_blank">- HEPDAK’ın Üye Olduğu CEENQA İçindeki Faaliyetleri: 2020</a>
+			</div>
 			<h4>- INQAAHE Faaliyetleri</h4>
 		{/*
 		<p>
