ab7d6eb8c5c366412d03f126b84d1707914e16c9
Samet Gölgeci <samet.golgeci@gmail.com>
2022-05-18 11:17:04 +0300

    v.4.35
    - a8_v1 siteye eklendi

--- pages/belgeler.js
+++ pages/belgeler.js
@@ -46,6 +46,10 @@ const Belgeler = () => (
                   <th scope="row">7</th>
                   <td>HEPDAK Arşivleme Yönergesi <Link href="doc/a7_v1.pdf"><a>Sürüm 1.0</a></Link> | <Link href="doc/a7_v2.pdf"><a>Sürüm 2.0</a></Link></td>
                 </tr>
+                <tr>
+                  <th scope="row">8</th>
+                  <td>HEPDAK Sürekli İyileştirme Komisyonu Yönergesi <strong>Sürüm 1.0</strong> (<Link href="doc/a8_v1.pdf"><a>PDF</a></Link> | <Link href="doc/a8_v1.docx"><a>Word</a></Link>)</td>
+                </tr>
               </tbody>
             </table>
             <table className="table table-striped">
