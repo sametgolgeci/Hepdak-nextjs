62731b63efc673c511b39e979a03b51585ce38e1
Samet Gölgeci <samet.golgeci@gmail.com>
2022-12-26 14:56:19 +0300

    v.4.74
    - Yeni dönem ücret girişleri yapıldı

--- pages/ucretler.js
+++ pages/ucretler.js
@@ -12,8 +12,8 @@ const Ucretler = () => (
 	    <div className="row akreditasyon">
 	    	<div className="col-md-12 akreditasyon-content">
           <div id="ucretler" className="akreditasyon-content-item">         
-            <div className="akreditasyon-content-subtitle">2022-2023 Değerlendirme Dönemi Akreditasyon Bedelleri</div>
-            <p>2022-2023 döneminde değerlendirilecek programlar için akreditasyon değerlendirme bedelleri aşağıdaki tabloda yer almaktadır.</p>
+            <div className="akreditasyon-content-subtitle">2023-2024 Değerlendirme Dönemi Akreditasyon Bedelleri</div>
+            <p>2023-2024 döneminde değerlendirilecek programlar için akreditasyon değerlendirme bedelleri aşağıdaki tabloda yer almaktadır.</p>
             <table className="table table-striped">
               <thead className="thead-dark">
                 <tr>
@@ -25,28 +25,28 @@ const Ucretler = () => (
               <tbody>
                 <tr>
                   <th scope="row">Genel Değerlendirme(1)</th>
-                  <td>42.000 TL + KDV*</td>
-                  <td>35.000 TL + KDV*</td>
+                  <td>79.000 TL + KDV*</td>
+                  <td>60.000 TL + KDV*</td>
                 </tr>
                 <tr>
                   <th scope="row">Ziyaretle Ara Değerlendirme(2)</th>
-                  <td>20.000 TL + KDV*</td>
-                  <td>15.000 TL + KDV*</td>
+                  <td>35.000 TL + KDV*/**</td>
+                  <td>30.000 TL + KDV*/**</td>
                 </tr>
                 <tr>
                   <th scope="row">Raporla Ara Değerlendirme(2)</th>
-                  <td>15.000 TL + KDV*</td>
-                  <td>10.000 TL + KDV*</td>
+                  <td>35.000 TL + KDV*</td>
+                  <td>30.000 TL + KDV*</td>
                 </tr>
                 <tr>
                   <th scope="row">Ziyaretle Kanıt Göster(3)</th>
-                  <td>20.000 TL + KDV*</td>
-                  <td>15.000 TL + KDV*</td>
+                  <td>30.000 TL + KDV*/**</td>
+                  <td>25.000 TL + KDV*/**</td>
                 </tr>
                 <tr>
                   <th scope="row">Raporla Kanıt Göster(3)</th>
-                  <td>15.000 TL + KDV*</td>
-                  <td>10.000 TL + KDV*</td>
+                  <td>35.000 TL + KDV*</td>
+                  <td>20.000 TL + KDV*</td>
                 </tr>
               </tbody>
             </table>
@@ -54,7 +54,9 @@ const Ucretler = () => (
             <p>(2) Daha önce yapılan genel değerlendirmesi sonucunda kısa süreli akreditasyon verilmiş olan programların değerlendirmesidir.</p>
             <p>(3) Daha önce yapılan ara veya genel değerlendirmesi sonucunda bir yıl süreli akreditasyon verilmiş olan programların değerlendirmesidir.</p>
             <p>* KDV miktarı %18 üzerinden hesaplanır.</p>
-            <div className="akreditasyon-content-subtitle">2022-2023 Değerlendirme Dönemi Akreditasyon Bedelleri Ödeme Planı</div>
+            <p>** Programın genel değerlendirmesi yüz yüze yapıldı ise, bu değerlendirmesi çevrimiçi yapılabilir. Fakat değerlendirme
+takımının gereksinim belirtmesi durumunda ziyaretin bir bölümü yüz yüze yapılabilir.</p>
+            <div className="akreditasyon-content-subtitle">2023-2024 Değerlendirme Dönemi Akreditasyon Bedelleri Ödeme Planı*/**</div>
             <table className="table table-striped">
               <thead className="thead-dark">
                 <tr>
@@ -65,77 +67,93 @@ const Ucretler = () => (
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">Eylül 2022 Taksiti</th>
+                  <th scope="row">Haziran 2023 Taksiti</th>
+                  <td>10.000 TL.+ KDV</td>
+                  <td>10.000 TL.+ KDV</td>
+                </tr>
+                <tr>
+                  <th scope="row">Ağustos 2023 Taksiti</th>
+                  <td>30.000 TL.+ KDV</td>
+                  <td>15.000 TL.+ KDV</td>
+                </tr>
+                <tr>
+                  <th scope="row">Ekim 2023 Taksiti</th>
                   <td>20.000 TL.+ KDV</td>
                   <td>15.000 TL.+ KDV</td>
                 </tr>
                 <tr>
-                  <th scope="row">Ekim 2022 Taksiti</th>
-                  <td>12.000 TL.+ KDV</td>
-                  <td>10.000 TL.+ KDV</td>
-                </tr>
-                <tr>
-                  <th scope="row">Kasım 2022 Taksiti</th>
-                  <td>10.000 TL.+ KDV</td>
-                  <td>10.000 TL.+ KDV</td>
+                  <th scope="row">Kasım 2023 Taksiti</th>
+                  <td>19.000 TL.+ KDV</td>
+                  <td>20.000 TL.+ KDV</td>
                 </tr>
                 <tr>
                   <th scope="row">TOPLAM</th>
-                  <td>42.000 TL.+ KDV</td>
-                  <td>35.000 TL.+ KDV</td>
+                  <td>79.000 TL.+ KDV</td>
+                  <td>60.000 TL.+ KDV</td>
                 </tr>
               </tbody>
               <thead className="thead-dark">
                 <tr>
-                  <th scope="col">Ziyaretle Ara Değerlendirme/Ziyaretle Kanıt Göster*</th>
+                  <th scope="col">Ziyaretle Ara Değerlendirme/Ziyaretle Kanıt Göster</th>
                   <th scope="col">Normal Öğretim</th>
                   <th scope="col">İkinci Öğretim</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">Ekim 2022 Taksiti</th>
+                  <th scope="row">Haziran 2023 Taksiti</th>
                   <td>10.000 TL.+ KDV</td>
-                  <td>7.500 TL.+ KDV</td>
+                  <td>10.000 TL.+ KDV</td>
                 </tr>
                 <tr>
-                  <th scope="row">Kasım 2022 Taksiti</th>
+                  <th scope="row">Ağustos 2022 Taksiti</th>
+                  <td>15.000 TL.+ KDV</td>
                   <td>10.000 TL.+ KDV</td>
-                  <td>7.500 TL.+ KDV</td>
+                </tr>
+                <tr>
+                  <th scope="row">Ekim 2022 Taksiti</th>
+                  <td>10.000 TL.+ KDV</td>
+                  <td>10.000 TL.+ KDV</td>
                 </tr>
                 <tr>
                   <th scope="row">TOPLAM</th>
-                  <td>20.000 TL.+ KDV**</td>
-                  <td>15.000 TL.+ KDV**</td>
+                  <td>35.000 TL.+ KDV**</td>
+                  <td>30.000 TL.+ KDV**</td>
                 </tr>
               </tbody>
               <thead className="thead-dark">
                 <tr>
-                  <th scope="col">Raporla Ara Değerlendirme/Raporla Kanıt Göster* </th>
+                  <th scope="col">Raporla Ara Değerlendirme/Raporla Kanıt Göster</th>
                   <th scope="col">Normal Öğretim</th>
                   <th scope="col">İkinci Öğretim</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">Ekim 2022 Taksiti</th>
-                  <td>7.500 TL.+ KDV</td>
-                  <td>5.000 TL.+ KDV</td>
+                  <th scope="row">Haziran 2023 Taksiti</th>
+                  <td>10.000 TL.+ KDV</td>
+                  <td>10.000 TL.+ KDV</td>
                 </tr>
                 <tr>
-                  <th scope="row">Kasım 2022 Taksiti</th>
-                  <td>7.500 TL.+ KDV</td>
+                  <th scope="row">Ağustos 2022 Taksiti</th>
+                  <td>15.000 TL.+ KDV</td>
+                  <td>10.000 TL.+ KDV</td>
+                </tr>
+                <tr>
+                  <th scope="row">Ekim 2022 Taksiti</th>
+                  <td>10.000 TL.+ KDV</td>
                   <td>5.000 TL.+ KDV</td>
                 </tr>
                 <tr>
                   <th scope="row">TOPLAM</th>
-                  <td>15.000 TL.+ KDV**</td>
-                  <td>10.000 TL.+ KDV**</td>
+                  <td>35.000 TL.+ KDV**</td>
+                  <td>25.000 TL.+ KDV**</td>
                 </tr>
               </tbody>
             </table>
             <p>* Ödeme planını takip etmek kurumun sorumluluğudur.</p>
-            <p>** Programların talep etmesi durumunda akreditasyon bedeli Kasım ayında tek seferde tahsil edilebilir.</p>
+            <p>** Öninceleme sonrası ziyaret edilmeme kararı alınan programların ödemiş oldukları Haziran taksit tutarı geri ödenmez.</p>
+            <p>- Toplu ödeme yapmak isteyen programların, HEPDAK Saymanı ile iletişime geçmesi gerekmektedir.</p>
           </div>
 	    	</div>
 	    </div>
