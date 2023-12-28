c6cd8a0c6eb01bc8c38fe43a71551afd3c24cb49
Samet Gölgeci <samet.golgeci@gmail.com>
2023-12-28 03:55:03 +0300

    v.5.82
    - Paydaş toplantıları 2022 dosyaları eklendi

--- pages/paydas-toplantilari.js
+++ pages/paydas-toplantilari.js
@@ -17,20 +17,21 @@ const Paydas = () => (
 						<tr>
 	          	<th scope="row" width="10px"></th>
 	          	<td>
-	          		<Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı-yoneticileri_toplanti/03_1.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantı Duyurusu - 3.1 (02 Ekim 2023)</a></Link><br/>
-	          		<Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı-yoneticileri_toplanti/03_2.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 3.2 (19 Ekim 2023)</a></Link>
+	          		<Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/03_1.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantı Duyurusu - 3.1 (02 Ekim 2023)</a></Link><br/>
+	          		<Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/03_2.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 3.2 (19 Ekim 2023)</a></Link>
 	          	</td>
 	        	</tr>
 	        	<tr>
 		          <th scope="row" width="10px"></th>
 		          <td>
-			          <Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı-yoneticileri_toplanti/02_1.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 2.1 (13 Ekim 2022)</a></Link><br/>
-			          <Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı-yoneticileri_toplanti/02_2.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 2.2 (13 Ekim 2022)</a></Link>
+		          	<Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/02_1.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantı Duyurusu - 2.1 (16 Şubat 2022)</a></Link><br/>
+			          <Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/02_2.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 2.2 (13 Ekim 2022)</a></Link><br/>
+			          <Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/02_3.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 2.3 (13 Ekim 2022)</a></Link>
 		          </td>
 		        </tr>
 		        <tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı-yoneticileri_toplanti/01.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 1 (24 Şubat 2022)</a></Link></td>
+		          <td><Link href="doc/paydas_toplantilari/hemsirelik_lisans_programı_yoneticileri_toplanti/01.pdf"><a target="_blank">Hemşirelik Lisans Programı Yöneticileri Toplantısı - 1 (24 Şubat 2022)</a></Link></td>
 		        </tr>
 					</tbody>
 				</table>
@@ -50,8 +51,9 @@ const Paydas = () => (
 		        <tr>
 		          <th scope="row" width="10px"></th>
 		          <td>
-			          <Link href="doc/paydas_toplantilari/basvuran_kurum_yoneticileri_toplanti/02.pdf"><a target="_blank">2022-2023 Dönemi HEPDAK Akreditasyon için Başvuran Kurumlara Yönelik Bilgilendirme Toplantısı (01 Mart 2022)</a></Link><br/>
-			          <Link href="doc/paydas_toplantilari/basvuran_kurum_yoneticileri_toplanti/02.ppt"><a target="_blank">2022-2023 Dönemi Akreditasyon için Başvuran Kurumlar Sunumu (01 Mart 2022)</a></Link>
+		          	<Link href="doc/paydas_toplantilari/basvuran_kurum_yoneticileri_toplanti/02_1.pdf"><a target="_blank">2022-2023 Dönemi HEPDAK Akreditasyon için Başvuran Kurumlara Yönelik Bilgilendirme Toplantı Duyurusu (17 Şubat 2022)</a></Link><br/>
+			          <Link href="doc/paydas_toplantilari/basvuran_kurum_yoneticileri_toplanti/02_2.pdf"><a target="_blank">2022-2023 Dönemi HEPDAK Akreditasyon için Başvuran Kurumlara Yönelik Bilgilendirme Toplantısı (01 Mart 2022)</a></Link><br/>
+			          <Link href="doc/paydas_toplantilari/basvuran_kurum_yoneticileri_toplanti/02_3.ppt"><a target="_blank">2022-2023 Dönemi Akreditasyon için Başvuran Kurumlar Sunumu (01 Mart 2022)</a></Link>
 		          </td>
 		        </tr>
 		        <tr>
@@ -67,15 +69,18 @@ const Paydas = () => (
 					<tbody>
 						<tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/akreditasyon_ziyaret-edilen_kurum_yonetici_toplanti/03.pdf"><a target="_blank">2022-2023 Dönemi Akreditasyon İçin Ziyaret Yapılan Kurumlar ile Toplantı (13 Mart 2023)</a></Link></td>
+		          <td><Link href="doc/paydas_toplantilari/akreditasyon_ziyaret_edilen_kurum_yonetici_toplanti/03.pdf"><a target="_blank">2022-2023 Dönemi Akreditasyon İçin Ziyaret Yapılan Kurumlar ile Toplantı Duyurusu (13 Mart 2023)</a></Link></td>
 		        </tr>
 		        <tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/akreditasyon_ziyaret-edilen_kurum_yonetici_toplanti/02.pdf"><a target="_blank">2021-2022 Dönemi Akreditasyon İçin Ziyaret Yapılan Kurumlar ile Toplantı (15 Mart 2022)</a></Link></td>
+		          <td>
+		          	<Link href="doc/paydas_toplantilari/akreditasyon_ziyaret_edilen_kurum_yonetici_toplanti/02_1.pdf"><a target="_blank">2021-2022 Dönemi Akreditasyon İçin Ziyaret Yapılan Kurumlar ile Toplantı Duyurusu (17 Şubat 2022)</a></Link><br/>
+		          	<Link href="doc/paydas_toplantilari/akreditasyon_ziyaret_edilen_kurum_yonetici_toplanti/02_2.pdf"><a target="_blank">2021-2022 Dönemi Akreditasyon İçin Ziyaret Yapılan Kurumlar ile Toplantısı (15 Mart 2022)</a></Link>
+		          </td>
 		        </tr>
 		        <tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/akreditasyon_ziyaret-edilen_kurum_yonetici_toplanti/01.pdf"><a target="_blank">2020-2021 Dönemi Ziyaret Yapılan Kurum Temsilcileri ile Toplantı (08 Mart 2021)</a></Link></td>
+		          <td><Link href="doc/paydas_toplantilari/akreditasyon_ziyaret_edilen_kurum_yonetici_toplanti/01.pdf"><a target="_blank">2020-2021 Dönemi Ziyaret Yapılan Kurum Temsilcileri ile Toplantısı (08 Mart 2021)</a></Link></td>
 		        </tr>
 					</tbody>
 				</table>
@@ -113,7 +118,11 @@ const Paydas = () => (
 					<tbody>
 						<tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/degerlendirme_takimlari_toplanti/02.pdf"><a target="_blank">HEPDAK Değerlendirme Takım Üyeleri Toplantısı (11 Ocak 2023)</a></Link></td>
+		          <td><Link href="doc/paydas_toplantilari/degerlendirme_takimlari_toplanti/03.pdf"><a target="_blank">HEPDAK Değerlendirme Takım Üyeleri Toplantısı - 2 (11 Ocak 2023)</a></Link></td>
+		        </tr>
+		        <tr>
+		          <th scope="row" width="10px"></th>
+		          <td><Link href="doc/paydas_toplantilari/degerlendirme_takimlari_toplanti/02.pdf"><a target="_blank">HEPDAK Değerlendirme Takım Üyeleri Toplantısı - 1 (06 Ocak 2022)</a></Link></td>
 		        </tr>
 						<tr>
 		          <th scope="row" width="10px"></th>
@@ -128,7 +137,11 @@ const Paydas = () => (
 					<tbody>
 						<tr>
 		          <th scope="row" width="10px"></th>
-		          <td><Link href="doc/paydas_toplantilari/ogrenci_degerlendiriciler_toplanti/01.pdf"><a target="_blank">HEPDAK Öğrenci Değerlendiricileri Toplantısı (25 Ocak 2023)</a></Link></td>
+		          <td><Link href="doc/paydas_toplantilari/ogrenci_degerlendiriciler_toplanti/02.pdf"><a target="_blank">HEPDAK Öğrenci Değerlendiricileri Toplantısı - 2 (25 Ocak 2023)</a></Link></td>
+		        </tr>
+		        <tr>
+		          <th scope="row" width="10px"></th>
+		          <td><Link href="doc/paydas_toplantilari/ogrenci_degerlendiriciler_toplanti/01.pdf"><a target="_blank">HEPDAK Öğrenci Değerlendiricileri Toplantısı - 1 (18 Ocak 2022)</a></Link></td>
 		        </tr>
 					</tbody>
 				</table>
