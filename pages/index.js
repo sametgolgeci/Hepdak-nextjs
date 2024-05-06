42b0b85cc5c4542c3d48534e158a523e94628d74
Samet Gölgeci <samet.golgeci@gmail.com>
2024-05-06 16:25:24 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ const Anasayfa = () => (
                 <div className="carousel-item active">
                   <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-ogrenci-degerlendirici-duyuru.jpeg"/></div>
                 </div>
-                <div className="carousel-item active">
+                <div className="carousel-item">
                   <Link href="/doc/hemed-hepdak-gorus.pdf"><a target="_blank">
                     <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-hemed-hepdak-gorus.jpeg"/></div>
                   </a></Link>
