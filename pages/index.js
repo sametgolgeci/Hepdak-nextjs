8c21454819fe5353611dadac861e15afd27ec77d
Samet Gölgeci <samet.golgeci@gmail.com>
2024-06-25 17:35:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -50,9 +50,6 @@ const Anasayfa = () => (
             <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
               <div className="carousel-inner">
                 <div className="carousel-item active">
-                  <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-kurban-bayrami.jpeg"/></div>
-                </div>
-                <div className="carousel-item">
                   <Link href="/doc/hemed-hepdak-gorus.pdf"><a target="_blank">
                     <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-hemed-hepdak-gorus.jpeg"/></div>
                   </a></Link>
