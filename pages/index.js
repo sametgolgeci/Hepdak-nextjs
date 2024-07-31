004d59f19e6825b0cad8c9ccf9693563fc785e12
Samet Gölgeci <samet.golgeci@gmail.com>
2024-07-31 23:49:41 +0300

    v.6.70
    - Vefat duyurusu

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,9 @@ const Anasayfa = () => (
             <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
               <div className="carousel-inner">
                 <div className="carousel-item active">
+                  <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-vefat-gulsevim-ceviker.jpeg"/></div>
+                </div>
+                <div className="carousel-item">
                   <Link href="/doc/hemed-hepdak-gorus.pdf"><a target="_blank">
                     <div className="main-top-one-content-full"><img className="slider-full-img" src="img/banner/2024-hemed-hepdak-gorus.jpeg"/></div>
                   </a></Link>
