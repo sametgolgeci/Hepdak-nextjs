ee6957c681b73484c3740354f04de381335cab42
Samet Gölgeci <samet.golgeci@gmail.com>
2024-12-29 22:58:19 +0300

    v.6.104
    - İngilizce Komisyon Toplantıları sayfası oluşturuldu

--- components/navigation-eng.js
+++ components/navigation-eng.js
@@ -56,7 +56,7 @@ const Navigation = () => (
 					<a className="nav-link dropdown-toggle diss header-nav-link" data-toggle="dropdown">MEETINGS</a>
 					<div className="dropdown-menu">
 						<Link href="#"><a className="dropdown-item">HEAK Meetings</a></Link>
-						<Link href="#"><a className="dropdown-item">Consistency Committee Meetings</a></Link>
+						<Link href="en/commission-meetings"><a className="dropdown-item">Commission Meetings</a></Link>
 						<Link href="#"><a className="dropdown-item">Team Leader Meetings</a></Link>
 						<Link href="#"><a className="dropdown-item">Standard Setting and Development Committee Meetings</a></Link>
 						<Link href="#"><a className="dropdown-item">International Meetings and Activities</a></Link>
---
+++ pages/en/commission-meetings.js
@@ -0,0 +1,46 @@
+import MasterPage from '../../components/masterpage-eng'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const Belgeler = () => (
+    <MasterPage>
+      <Head>
+        <title>HEPDAK | Commission Meetings</title>
+      </Head>
+      <div className="genel komisyon-paydas">
+        <h1>Commission Meetings</h1>
+        <h5><a data-toggle="collapse" href="#CIC" role="button" aria-expanded="false" aria-controls="CIC">Continuous Improvement Commission (CIC)</a></h5>
+        <div className="collapse" id="CIC">
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <td><Link href="doc/en/commission_meetings/CIC/2024.pdf"><a>2024 Annual Report</a></Link></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h5><a data-toggle="collapse" href="#WDC" role="button" aria-expanded="false" aria-controls="WDC">Web Design Commission (WDC)</a></h5>
+        <div className="collapse" id="WDC">
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <td><Link href="doc/en/commission_meetings/WDC/2024-02.pdf"><a>2024 Annual Report for the Period July-December</a></Link></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h5><a data-toggle="collapse" href="#EC" role="button" aria-expanded="false" aria-controls="EC">Education Commission (EC)</a></h5>
+        <div className="collapse" id="EC">
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <td><Link href="doc/en/commission_meetings/EC/2024.pdf"><a>2024 Annual Report for the Period July-December</a></Link></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="safe"></div>
+    </MasterPage>
+)
+export default Belgeler
