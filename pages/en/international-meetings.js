bbf9d74f8f487370b6b51f90ba1df43b759999b0
Samet Gölgeci <samet.golgeci@gmail.com>
2025-01-07 05:49:51 +0300

    - Düzeltme
    - Düzeltme

--- pages/en/international-meetings.js
+++ pages/en/international-meetings.js
@@ -33,7 +33,7 @@ const Symposium = () => (
 
       <h4>- INQAAHE Meetings and Activities</h4>
       <div className="madde-p">
-        <a className="uluslararasi-link" data-toggle="collapse" href="#collapseINQAAHE2024" role="button" aria-expanded="false" aria-controls="collapseINQAAHE2024">- INQAAHE Toplantısı (2024)</a>
+        <a className="uluslararasi-link" data-toggle="collapse" href="#collapseINQAAHE2024" role="button" aria-expanded="false" aria-controls="collapseINQAAHE2024">- 2024 INQAAHE Meeting Participants</a>
         <div className="collapse" id="collapseINQAAHE2024">
           <p>The HEPDAK International Relations Commission carried out its work in the second half of 2024 in accordance with its mandate. The Commission followed the meetings of the accreditation organization INQAAHE (The International Network for Quality Assurance Agencies in Higher Education), of which HEPDAK is a member. The Commission attended the INQAAHE meeting on October 22. Commission member Sergül Duygulu was invited by the Romanian ARACIS (Agency for Quality Assurance in Higher Education) as an international expert for the institutional evaluation (06-08 November 2024 / Grigore T. Popa College of Medicine and Pharmacy Iaşi - UMF “Grigore T. Popa” Iaşi).</p>
           <p>
