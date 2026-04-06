e98d672caeb65bf0174f0e1655f8bada5e945f04
Samet Gölgeci <samet.golgeci@gmail.com>
2026-04-06 17:37:41 +0300

    Update rapor-hazirlama.js

--- pages/rapor-hazirlama.js
+++ pages/rapor-hazirlama.js
@@ -11,8 +11,8 @@ const Rapor = () => (
         <h1>Rapor Hazırlama</h1>
         <p className="madde-p">Ara Rapor Hazirlarken Dikkat Edilmesi Gereken Konular <Link href="doc/b8_v2.pdf">Sürüm 2.0</Link></p>
         <p className="madde-p">Özdeğerlendirme Raporu Hazırlarken Dikkat Edilmesi Gereken Noktalar <Link href="doc/b9_v2.pdf">Sürüm 2.0</Link></p>
-        <p className="madde-p">HEPDAK Öğrenci Değerlendirici Raporu <strong>Sürüm 4.0</strong> (<Link href="doc/b12_v4.pdf">PDF</Link> | <Link href="doc/b12_v4.doc">Word</Link>)</p>
-        <p className="madde-p">HEPDAK Akredite Eğitim Programları İçin Gelişim Raporu Hazırlama Rehberi <strong>Sürüm 2.2</strong> (<Link href="doc/b13_v2_2.pdf">PDF</Link> | <Link href="doc/b13_v2_2.docx">Word</Link>)</p>
+        <p className="madde-p">HEPDAK Öğrenci Değerlendirici Raporu <Link href="doc/b12_v6.pdf">Sürüm 6.0</Link></p>
+        <p className="madde-p">HEPDAK Akredite Eğitim Programları İçin Gelişim Raporu Hazırlama Rehberi <strong>Sürüm 2.3</strong> (<Link href="doc/b13_v2_3.pdf">PDF</Link> | <Link href="doc/b13_v2_3.docx">Word</Link>)</p>
         <p className="madde-p">HEPDAK Raporları Yazım Kuralları <Link href="doc/c1_v1.pdf">Sürüm 1.0</Link></p>  
       </div>
       <div className="safe"></div>
